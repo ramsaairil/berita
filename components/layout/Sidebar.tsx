6a6e9ccd4130b35bc3567d12eded13d8eba7c489
Ramsa <ramsaairil05@gmail.com>
2026-06-19 20:46:37 +0700

    feat: add trending news page and layout sidebar component

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import TrendingWidget from "@/components/features/articles/TrendingWidget";
 import { getCategoryColor } from "@/lib/categoryColors";
 
 interface Category {
@@ -12,8 +11,6 @@ export default function Sidebar({ categories }: { categories: Category[] }) {
   return (
     <aside className="hidden lg:block w-[35%] shrink-0">
       <div className="flex flex-col gap-8 sticky top-24">
-        {/* Trending Widget */}
-        <TrendingWidget />
       </div>
     </aside>
   );
